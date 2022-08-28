368d2a53c2253a3d7a6e9a188c3bb76002c0a980
unleftie <23164223+unleftie@users.noreply.github.com>
2022-08-28 21:15:11 +0300

    added firewall rule for web

--- firewall.tf
+++ firewall.tf
@@ -33,21 +33,21 @@ resource "hcloud_firewall" "vpn" {
   }
 }
 
-# resource "hcloud_firewall" "web" {
-#   name   = "${var.service_name}-firewall-web"
-#   labels = var.labels
+resource "hcloud_firewall" "web" {
+  name   = "${var.service_name}-firewall-web"
+  labels = var.labels
 
-#   rule {
-#     direction  = "in"
-#     protocol   = "tcp"
-#     source_ips = [var.personal_ip1]
-#     port       = "80"
-#   }
+  rule {
+    direction  = "in"
+    protocol   = "tcp"
+    source_ips = ["0.0.0.0/0"]
+    port       = "80"
+  }
 
-#   rule {
-#     direction  = "in"
-#     protocol   = "tcp"
-#     source_ips = [var.personal_ip1]
-#     port       = "443"
-#   }
-# }
+  rule {
+    direction  = "in"
+    protocol   = "tcp"
+    source_ips = ["0.0.0.0/0"]
+    port       = "443"
+  }
+}
