3d2b025d3266603c1a893cc1f7f5f6bd1eb78e24
unleftie <23164223+unleftie@users.noreply.github.com>
2022-09-06 10:49:39 +0300

    added knocking rule

--- main.tf
+++ main.tf
@@ -1,15 +1,14 @@
-resource "hcloud_server" "server1" {
-  name         = var.server_name
+resource "hcloud_server" "server2" {
+  name         = var.server_name2
   server_type  = var.server_type
   location     = var.server_location
   image        = var.server_image
   labels       = var.labels
   ssh_keys     = [hcloud_ssh_key.ssh1.id]
-  firewall_ids = [hcloud_firewall.ssh.id, hcloud_firewall.icmp.id, hcloud_firewall.vpn.id, hcloud_firewall.web.id]
+  firewall_ids = [hcloud_firewall.ssh.id, hcloud_firewall.icmp.id, hcloud_firewall.vpn.id, hcloud_firewall.knocking.id]
 
   network {
     network_id = hcloud_network.network1.id
-    # ip          = "10.10.10.10"
   }
 
   public_net {
@@ -20,4 +19,10 @@ resource "hcloud_server" "server1" {
   depends_on = [
     hcloud_network_subnet.subnet1
   ]
+
+  lifecycle {
+    ignore_changes = [
+      network
+    ]
+  }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
-output "ipv4_address" {
-  value       = hcloud_server.server1.ipv4_address
+output "ipv4_address2" {
+  value       = hcloud_server.server2.ipv4_address
   sensitive   = false
   description = "Public ipv4 address"
 }
