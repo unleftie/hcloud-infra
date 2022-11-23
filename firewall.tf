1af720a0a711ea6ca5daf0121606aae64fa3cf40
unleftie <23164223+unleftie@users.noreply.github.com>
2022-11-23 17:24:12 +0200

    fix firewall internal rule

--- firewall.tf
+++ firewall.tf
@@ -1,3 +1,41 @@
+
+resource "hcloud_firewall" "internal" {
+  name   = "${var.service_name}-firewall-internal"
+  labels = var.labels
+
+  rule {
+    direction  = "in"
+    protocol   = "icmp"
+    source_ips = [var.network_cidr_block1]
+  }
+
+  rule {
+    direction  = "in"
+    protocol   = "tcp"
+    port       = "any"
+    source_ips = [var.network_cidr_block1]
+  }
+
+  rule {
+    direction  = "in"
+    protocol   = "udp"
+    port       = "any"
+    source_ips = [var.network_cidr_block1]
+  }
+
+  rule {
+    direction  = "in"
+    protocol   = "gre"
+    source_ips = [var.network_cidr_block1]
+  }
+
+  rule {
+    direction  = "in"
+    protocol   = "esp"
+    source_ips = [var.network_cidr_block1]
+  }
+}
+
 resource "hcloud_firewall" "icmp" {
   name   = "${var.service_name}-firewall-icmp"
   labels = var.labels
