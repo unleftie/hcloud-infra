f4130ea23c29d2d20c75b1d4d260e8d39dad774d
unleftie <23164223+unleftie@users.noreply.github.com>
2023-12-25 22:49:54 +0200

    fix: fw rules

--- main.tf
+++ main.tf
@@ -50,9 +50,9 @@ resource "hcloud_server" "server3" {
   firewall_ids = [
     hcloud_firewall.internal.id,
     hcloud_firewall.icmp.id,
-    hcloud_firewall.ssh.id,
     hcloud_firewall.web.id,
     hcloud_firewall.vpn.id,
+    hcloud_firewall.ssh_knocking.id,
   ]
 
   network {
@@ -89,8 +89,8 @@ resource "hcloud_server" "server4" {
   firewall_ids = [
     hcloud_firewall.internal.id,
     hcloud_firewall.icmp.id,
-    hcloud_firewall.ssh.id,
     hcloud_firewall.cloudflare.id,
+    hcloud_firewall.ssh_knocking.id,
   ]
 
   network {
