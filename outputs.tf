0ce30e6db30ce35cebb46b2375f87ec6a2ab3e46
denysrondaliev <denysrondaliev@gmail.com>
2024-02-03 15:57:50 +0200

    fix: private ip4 output

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,17 @@
 output "ip4" {
+  description = "Servers ipv4 addresses"
   value = {
     try(hcloud_server.server3[0].name, null) = try(hcloud_server.server3[0].ipv4_address, null)
     try(hcloud_server.server4[0].name, null) = try(hcloud_server.server4[0].ipv4_address, null)
     try(hcloud_server.server5[0].name, null) = try(hcloud_server.server5[0].ipv4_address, null)
   }
-  description = "Servers ipv4 addresses"
+}
+
+output "private_ip4" {
+  description = "Servers private ipv4 addresses"
+  value = {
+    try(hcloud_server.server3[0].name, null) = try(one(hcloud_server.server3[0].network[*]).ip, null)
+    try(hcloud_server.server4[0].name, null) = try(one(hcloud_server.server4[0].network[*]).ip, null)
+    try(hcloud_server.server5[0].name, null) = try(one(hcloud_server.server5[0].network[*]).ip, null)
+  }
 }
