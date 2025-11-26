52294bdb53bfa258aa7bb27915d77078352d78d0
unleftie <denysrondaliev@gmail.com>
2025-11-26 21:46:28 +0200

    feat: added dns records

--- zone.tf
+++ zone.tf
@@ -2,9 +2,27 @@ resource "hcloud_zone" "zone1" {
   name = var.zone1_name
   mode = "primary"
 
-  ttl = 300
+  ttl = 10800
 
   delete_protection = false
+
+  labels = var.labels
+}
+
+resource "hcloud_zone_rrset" "api1" {
+  zone = hcloud_zone.zone1.name
+  name = "api"
+  type = "A"
+
+  ttl = 300
+
+  records = [
+    {
+      value = try(hcloud_server.server6[0].ipv4_address, null), comment = "api server 1"
+    },
+  ]
+
+  change_protection = false
 
   labels = var.labels
 }
